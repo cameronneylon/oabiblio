39938eed63736adf056b80dfc4518ac556b8016b
Ana Nelson <ana@ananelson.com>
2012-08-25 13:51:32 -0700

    Add modargs to oacensus_commands. Define skeleton update and report commands.

--- oabiblio/oacensus_commands.py
+++ oabiblio/oacensus_commands.py
@@ -1,2 +1,29 @@
+from modargs import args
+import sys
+
+MOD = sys.modules[__name__]
+PROG = 'oacensus'
+DEFAULT_COMMAND = 'report'
+
 def run():
-    print "This is oacensus!"
+    args.parse_and_run_command(sys.argv[1:], MOD, default_command=DEFAULT_COMMAND)
+
+def update_command(
+        foo='bar' # This is a command line argument
+        ):
+    """
+    Downloads the latest source data files from sources.
+    """
+    print "this is the update command! foo is %s" % foo
+
+def report_command():
+    """
+    Generates the report.
+    """
+    print "this is the report command!"
+
+def help_command(on=False):
+    args.help_command(PROG, MOD, DEFAULT_COMMAND, on)
+
+def help_text(on=False):
+    return args.help_text(PROG, MOD, DEFAULT_COMMAND, on)
