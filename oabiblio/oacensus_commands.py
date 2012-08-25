ed19983dcd889ceb5e7e0e68640aa9155526a1e7
Ana Nelson <ana@ananelson.com>
2012-08-25 14:06:02 -0700

    Add parameters to oacensus update for selecting which sources to update.

--- oabiblio/oacensus_commands.py
+++ oabiblio/oacensus_commands.py
@@ -11,13 +11,28 @@ def run():
     args.parse_and_run_command(sys.argv[1:], MOD, default_command=DEFAULT_COMMAND)
 
 def update_command(
+        pubmed=True, # whether to update from pubmed
+        crossref=True, # whether to update from crossref
+        doaj=True, # whether to update from doaj
         years="11,12" # comma separated list of years
         ):
     """
     Downloads the latest source data files from sources.
     """
+    # Do any extra processing of praams.
     years = years.split(",")
-    crossref(years)
+
+    if pubmed:
+        # TODO update pubmed
+
+    if crossref:
+        # TODO update crossref
+        crossref(years)
+
+    if doaj:
+        # TODO update doaj
+
+    print "updating is complete!"
 
 def report_command():
     """
